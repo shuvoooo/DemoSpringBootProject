68040e25037ce561dea1a487ff3f169895a5040c
shuvoooo <shuvo.punam@gmail.com>
2022-07-16 14:59:47 +0530

    Initial commit

---
+++ src/main/java/com/example/demospringbootproject/DemoSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootProjectApplication.class, args);
+    }
+
+}
